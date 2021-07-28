32801641b7d9c4f0586b21671c4b0a246f843414
Alexey Volegov <anvolegov87@gmail.com>
2021-07-28 15:04:52 +0300

    new lessons

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace D_OOP
 {
@@ -6,6 +7,123 @@ namespace D_OOP
     {
 
         static void Main(string[] args)
+        {
+            
+            
+
+
+        }
+        static void BoxingUnboxing()
+        {
+            int x = 1;
+            // boxing
+            object obj = x; // здесь происходит следующее: object оборачивает значение х, как бы переносит значение в кучу, а obj- это
+                            // ссылка на значение 
+
+            // unboxing
+            int y = (int)obj;
+
+            // однако с боксингом и анбоксингом есть кое-какие проблемы
+
+            double pi = 3.14;
+            object obj1 = pi;
+            // кто-либо, работающий с этим objectом может быть и не в курсе, что там был ранее double, и попробует скаставать из него int
+            int number = (int)obj1; // если это запустить- получим исключени
+                                    // в этом и есть проблема object, то есть никто не знает какой именно тип там лежит
+            Console.WriteLine(number);
+            // изнначально object задумывался как нечто универсальное бля перебачи каких угодно аргументов, но из-за проблемы описанной выше,
+            // работать с ним было не очень удобно и с появлением дженериков с object уже никто не работает.
+            // Однако если у нас есть некий метод, который принимает object в качестве аргументов, мы все таки можем узнать типы принимаемых.
+            // см. метод Do()
+        }
+        static void Do(object obj) 
+        {
+            // на примере PointRef
+            bool isPointRef = obj is PointRef;
+            if (isPointRef)
+            {
+                PointRef pr = (PointRef)obj;
+                Console.WriteLine(pr.X);
+            }
+            else
+            {
+                // какой-нибудь код в случае если тип не PointRef
+            }
+
+            // второй способ более простой
+            PointRef pr1 = obj as PointRef;
+            // если obj является PointRef то произойдет кастование, а если нет, то в pr1 запишется null
+            if (pr1 != null)
+            {
+                Console.WriteLine(pr1.X);
+            }
+            else
+            {
+                // какой-либо код если в pr1 записан null
+            }
+        }
+        static void NullableValueTypesDemo()
+        {
+            //бывает случаи, например при получении данных из бд, когда нам нужно присвоить переменной null, ну то есть ничего,
+            //но просто так это сделать не получится по этому в таких случаях необходимо указывать ? после типа переменной
+            //в которую мы хотим поместить null
+            //например так если у нас тип сложный, то есть с использованием has value property:
+            PointVal? pv = null;
+            if (pv.HasValue)
+            {
+                Console.WriteLine(pv.Value.X);
+            }
+            else
+            {
+                //какой-то код, который будет выполняться в случае если записан null
+            }
+            // или так без has value property:
+            PointVal pv3 = pv.GetValueOrDefault();
+
+            //простые типы можно так записывать
+            int? a = null;
+            Console.WriteLine(a); // ничего не напечатает, так как мы ничего и не записали
+        }
+        static void PassByRefDemo()
+        {
+            int a = 1;
+            int b = 2;
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            Console.ReadLine();
+
+            var list = new List<int>();
+            AddNumbers(list);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            // в случае если нам нужна функция, которая сможет изменять исходные данные, необходимо пользоваться ключевым словом ref
+            // перед объявлением типа принимающихся аргументов, но только в случае если принимающиеся аргументы типа значения.
+            Console.WriteLine($"Original a={a}, b={b}");
+
+            int tmp = a;
+            a = b;
+            b = tmp;
+
+            Console.WriteLine($"Swapped a={a}, b={b}");
+
+
+        }
+        static void AddNumbers(List<int> numbers)
+        {
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+        }
+        static void ValRefTypesDemo()
         {
             // поведение при копировании в случае структур и классов
             PointVal a;
@@ -31,41 +149,18 @@ namespace D_OOP
 
             c.LogValues();
             d.LogValues();
-
-
+        }
+        static void StaticClassAndMethodsDemo()
+        {
             double result2 = Calculator_static.Average2(1, 2, 3);
-
-
-            Calculator calc3 = new Calculator();
-            if(calc3.TryDevide(10, 0, out double result))
-            {
-                Console.WriteLine(result);
-            }
-            else
-            {
-                Console.WriteLine("Failed to divide");
-            }
-
-            Console.WriteLine();
-
-            Character ch = new Character();
-            Console.WriteLine(ch.Health);
-            ch.Hit(20);
-            Console.WriteLine(ch.Health);
-
-            Console.WriteLine();
-
+        }
+        static void ClassMethodsAndOverloadedMethodsDemo()
+        {
             Calculator calc = new Calculator();
             double squere2 = calc.CalcTriangleSquere(10, 20); // вызывается второй метод
             double squere1 = calc.CalcTriangleSquere(5, 5, 5); // вызывается первый метод 
             Console.WriteLine($"Squere1={squere1} Squere2={squere2}");
 
-
-            Console.WriteLine();
-
-            Console.WriteLine(ParserRomeNumbers.Parse("XIX"));
-
-            Console.WriteLine();
             Calculator calc1 = new Calculator();
             double avg = calc1.Average(new int[] { 1, 2, 3, 4 });
             //или с params
@@ -73,12 +168,31 @@ namespace D_OOP
             Console.WriteLine(avg);
             Console.WriteLine();
             double sq = calc1.CalcTriangleSquere(ab: 10, ac: 20, alpha: 30);
-
-
-
-
-
         }
+        static void OutParametresDemo()
+        {
+            Calculator calc3 = new Calculator();
+            if (calc3.TryDevide(10, 0, out double result))
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Failed to divide");
+            }
+        }
+        static void ProtectedAtributesDemo() 
+        {
+            Character ch = new Character();
+            Console.WriteLine(ch.Health);
+            ch.Hit(20);
+            Console.WriteLine(ch.Health);
+        }
+        static void HomeWorkRomeParse()
+        {
+            Console.WriteLine(ParserRomeNumbers.Parse("XIX"));
+        }
+
     }
 }
 
