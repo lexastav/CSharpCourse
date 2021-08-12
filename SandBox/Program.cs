87f8c28e7748c63ab0ee6fb1bdc3741fb0b5d739
Alexey Volegov <anvolegov87@gmail.com>
2021-08-12 18:14:34 +0300

    new lessons

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -6,9 +6,56 @@ namespace Slide01
 	{
 		static void Main()
 		{
-		
-        }
 
+
+		}
+		static string RemoveWhiteSpace(string text)
+        {
+			int charInd = 0;
+			while (char.IsWhiteSpace(text[charInd]))
+			{
+				if (charInd == text.Length - 1)
+					return "";
+				charInd++;
+			}
+			return text.Substring(charInd++);
+		}
+		static void PowOfTwo()
+        {
+			var input = Console.ReadLine();
+			int pow = 0;
+			int number = int.Parse(input);
+			int result = 1;
+			while (true)
+			{
+				result = (int)Math.Pow(2, pow);
+				pow++;
+				if (result > number)
+					break;
+			}
+			Console.WriteLine(result);
+		}
+		static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+		{
+			bool shouldFire = true;
+			if (enemyInFront == true)
+			{
+				if (enemyName == "boss")
+				{
+					if (robotHealth < 50) shouldFire = false;
+					if (robotHealth > 100) shouldFire = true;
+				}
+			}
+			else
+			{
+				return false;
+			}
+			return shouldFire;
+		}
+		static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+		{
+			return enemyInFront && enemyName != "boss" || (enemyInFront && enemyName == "boss" && robotHealth > 49);
+		}
 		private static string GetMinX(int a, int b, int c)
 		{
 
@@ -32,7 +79,5 @@ namespace Slide01
 			var squareRoot = Math.Sqrt(discrimenant);
 			return (-b - squareRoot) / (2 * a);
         }
-
-
 	}
 }
