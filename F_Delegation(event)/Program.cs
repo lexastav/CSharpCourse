da2796cd629d7f6bc401a10fc2d38f229591368d
lexastav <lexastav@me.com>
2021-08-21 18:11:13 +0300

    add new lessons

---
+++ F_Delegation(event)/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Timers;
+
+namespace F_Delegation_event_
+{
+    public class Car
+    {
+        // сама конструкция с использование делегатов выглядит весьма запутаной, необходимо написание методов добавления и удаления,
+        // вот что бы упрастить подобного рода работу и придумали eventы. Event- базируется на delegate. Является как бы надстройкой.
+        int speed = 0;
+
+        //public event TooFast TooFastDriving;
+        //public delegate void TooFast(int currentspeed);
+
+        // однако в реальности на скорее всего не придется самостоятельно писать делегаты, так как все уже есть в библиотеке
+
+        public event Action<object, int> TooFastDriving;
+
+        public void Start()
+        {
+            speed = 10;
+        }
+        public void Accelerate()
+        {
+            speed += 10;
+
+            if (speed > 80)
+            {
+                if (TooFastDriving != null)
+                {
+                    TooFastDriving(this, speed);
+                }  
+            }
+
+        }
+        public void Stop()
+        {
+            speed = 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // на самом деле в BCL полно типов, которые базируются на event, например таймер, который позволяет нам завести таймер и подписаться
+            // на событие, которое срабатывает тогда, когда истек определенный период времени.
+            var timer = new Timer();
+            // и у него есть событие Elapsed
+            timer.Elapsed += Timer_Elapsed;
+
+
+            
+            
+            Console.ReadLine();
+            var car = new Car();
+            // так мы подписываемся на событие
+            car.TooFastDriving += HandleOnTooFast;
+            car.TooFastDriving += HandleOnTooFast;
+
+            // так мы отписываемся
+            car.TooFastDriving -= HandleOnTooFast;
+
+            car.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                car.Accelerate();
+            }
+        }
+        // обработчик события
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var timer = (Timer)sender;
+        }
+
+        private static void HandleOnTooFast(object obj, int speed)
+        {
+            Console.WriteLine($"Oh, I got it, calling brake! Current speed- {speed}");
+            var car = (Car)obj;
+            car.Stop();
+        }
+    }
+}
---
+++ F_Delegation/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace F_Delegation
+{
+    public class Car
+    {
+        int speed = 0;
+        // определим делегат, который описывает некую сигнатуру, и этой сигнатуре должен следовать обработчик события, он будет в клиентском коде
+        // в нашем случае обработчиком события будет метод private static void HandleOnTooFast()
+        public delegate void TooFast(int currentspeed);
+
+        //объявим экземпляр делегата
+        private TooFast tooFast;
+
+        public void Start()
+        {
+            speed = 10;
+        }
+        public void Accelerate()
+        {
+            speed += 10;
+
+            if (speed > 80)
+            {
+                // сам обработчик вызывается вот здесь
+                tooFast(speed);
+            }
+                
+        }
+        public void Stop()
+        {
+            speed = 0;
+        }
+        public void RegisterOnTooFast(TooFast tooFast)
+        {
+            // добавляем в список делегаты ссылку на метод-обработчик
+            this.tooFast += tooFast;
+        }
+
+        public void UnregisterOnTooFast(TooFast tooFast)
+        {
+            // а так можно осоединять обработчики
+            this.tooFast -= tooFast;
+        }
+    }
+    class Program
+    {
+        static Car car;
+        static void Main(string[] args)
+        {
+            car = new Car();
+            car.RegisterOnTooFast(HandleOnTooFast);
+            // вот если мы сделаем так, то в делегате будет 2 ссылки, а значит HandleOnTooFast вызовется 2 раза, то есть мы дважды подписались
+            car.RegisterOnTooFast(HandleOnTooFast);
+            // а так мы можем отписаться, и ввод останется только один
+            car.UnregisterOnTooFast(HandleOnTooFast);
+
+            car.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                car.Accelerate();
+            }           
+        }
+        private static void HandleOnTooFast(int speed)
+        {
+            Console.WriteLine($"Oh, I got it, calling brake! Current speed- {speed}");
+            car.Stop();
+        }
+    }
+}
